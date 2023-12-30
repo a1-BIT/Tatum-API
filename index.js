4cb214da4478a90f3bb8e16e1fc4c917844f7f67
a1-BIT <ayadav23198@gmail.com>
2023-12-30 22:22:17 +0530

    add crate tron wallet endpoint

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 4001;
+
+app.get("/createTronWallet", async (req, res) => {
+  try {
+    const resp = await fetch(`https://api.tatum.io/v3/tron/wallet`, {
+      method: "GET",
+      headers: {
+        "x-api-key": process.env.TATUM_API_KEY,
+      },
+    });
+
+    const data = await resp.json();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
